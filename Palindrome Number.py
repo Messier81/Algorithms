c432235cbe7fd65bd56cecf1c8a5807dd022d6eb
Messier81 <nesanyorku@gmail.com>
2017-12-21 22:24:02 -0500

    Better compact version that tests half the string

--- Palindrome Number.py
+++ Palindrome Number.py
@@ -1,29 +1,19 @@
 #Determine whether an integer is a palindrome. Do this without extra space.
 class Solution(object):
     def isPalindrome(self, x):
-        """
-        :type x: int
-        :rtype: bool
-        """
-
-        """
-        :Take absolute value to get rid of negative
-        """
-        x = abs(x)
-        if x < 10:
+        #If x is negative, it is not a palindrome..
+        #If x is less than 9, it is a plindrome.
+        #If x ends in 0, then if x is not 0, then it is not a palindrome.
+        if (x < 0 or ((x % 10 == 0) and (x != 0))): 
+            return False
+        elif (x >=0 and x <= 9):
             return True
-        
-        i = x
-        j = 0
-        
-        while i >= 1:
-            i = i / 10
-            j += 1
-            
-        loop_counter = int(j / 2)
-        
-        print (j)
-       
+        else:
+            num = 0
+            while x > num:
+                num = (num * 10) + int(x % 10)
+                x = int(x / 10)
+            return x == num or x == num / 10
 
 a = Solution()
-a.isPalindrome(21712);        
+print(a.isPalindrome(57075))          
